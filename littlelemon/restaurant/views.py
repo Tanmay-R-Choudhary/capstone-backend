9a584e350fbed43342a9a52efe53b46aec18d0c6
Tanmay-R-Choudhary <tanmay.d.choudhary@gmail.com>
2024-05-26 19:07:15 +0530

    added models and views for booking and menu items

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
+from rest_framework import generics
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
